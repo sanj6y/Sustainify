b5c709f6782184a11cf029c018b028d005f15c53
Sanjay Chandrasekar <sanjay.chandrasekar@gmail.com>
2023-02-03 21:49:24 -0800

    Initial Commit

---
+++ Sustainly/SustainlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SustainlyApp.swift
+//  Sustainly
+//
+//  Created by Sanjay Chandrasekar on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SustainlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
